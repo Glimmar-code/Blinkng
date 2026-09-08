2a9a628e882a9d2494f4d39ddd08a501c3b0cb57
Glimmar <golowosile@gmail.com>
2026-09-08 17:28:15 +0100

    Add Blink web runtime config

---
+++ web/config.js
@@ -0,0 +1,7 @@
+window.BLINK_WEB_CONFIG = Object.freeze({
+  supabaseUrl: "https://jhwgifrlxwspoedxjaly.supabase.co",
+  publishableKey: "sb_publishable_2lfIyTW9SwiTOvafGXp10Q_I7DwUs-J",
+  androidPackage: "com.aistudio.blink.appvtwo",
+  playStoreUrl: "https://play.google.com/store/apps/details?id=com.aistudio.blink.appvtwo",
+  siteUrl: ""
+});
