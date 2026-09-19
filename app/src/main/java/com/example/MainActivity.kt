61dc3f54f71bc55e22465da13d3833ecfa9946fa
Glimmar <golowosile@gmail.com>
2026-09-19 23:31:49 +0100

    Restore Android Build import used by Paystack flow

--- app/src/main/java/com/example/MainActivity.kt
+++ app/src/main/java/com/example/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example
 
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.BackHandler
